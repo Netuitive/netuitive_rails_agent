dd6ba68d49d78ea9729b402d2e11b534965fb86d
John King <john.king@netuitive.com>
2016-01-04 14:55:34 -0500

    counters

--- lib/netuitive/gc.rb
+++ lib/netuitive/gc.rb
@@ -4,9 +4,13 @@ class GCStatsCollector
 		if GC::Profiler.enabled?
 			begin
 				GC.stat.each do |key, value|
-					NetuitiveRubyAPI::netuitivedServer.aggregateMetric("GC.stat.#{key}", value)
+					if key == "total_allocated_object" or key == "total_freed_object" or key == "count"
+						NetuitiveRubyAPI::netuitivedServer.addCounterSample("GC.stat.#{key}", value)
+					else
+						NetuitiveRubyAPI::netuitivedServer.aggregateMetric("GC.stat.#{key}", value)
+					end
 				end
-				NetuitiveRubyAPI::netuitivedServer.aggregateMetric("GC.profiler.total_time", GC::Profiler.total_time)
+				NetuitiveRubyAPI::netuitivedServer.addCounterSample("GC.profiler.total_time", GC::Profiler.total_time)
 			rescue
 				NetuitiveLogger.log.error "failure to communicate to netuitived"
 			end
